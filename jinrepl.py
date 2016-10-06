75e60e0f892680e74c90675aa20919319756d0a1
jeronimog <garciaj.uk@gmail.com>
2016-10-06 21:43:40 +0100

    adding autocompletion for filters,  need more work

--- jinrepl.py
+++ jinrepl.py
@@ -1,9 +1,18 @@
-from jinja2 import Template
+import jinja2
 from termcolor import colored
-import readline
+import rlcompleter, readline
 import ast
 import sys
 
+
+
+def complete(text,state):
+        texttemp = text
+        if '|' in text:
+            results=[x for x in map(lambda x: x.replace("do_","") if x.startswith("do_") else x ,dir(jinja2.filters)) if x.startswith(text.split('|')[-1].replace(" ",""))]+[None]
+            return "|".join(text.split('|')[:-1]) + '| ' +  results[state]
+        else:
+            return [None]
 def parser(i):
     if "--" in i:
         code=i.split("--")[0]
@@ -13,11 +22,15 @@ def parser(i):
     try:
         if 'vars' not in locals():
             vars = {}
-        print colored('> ','green') +  Template(code).render(vars)
+        print colored('> ','green') +  jinja2.Template(code).render(vars)
     except Exception as e:
         print colored('e: ','red') , e
 
 def main():
+    readline.set_completer_delims('\t\t')
+    readline.parse_and_bind('tab: complete')
+    readline.set_completer(complete)
+
     try:
         while True:
             i = raw_input('jinrepl' + colored('> ','red'))
